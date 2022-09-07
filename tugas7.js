378f2d699b5ce93fc224952f5f4ce0cb61ada9fb
farraz13 <farraz.f13@gmail.com>
2022-09-07 21:44:40 +0700

    c7

---
+++ tugas7.js
@@ -0,0 +1,15 @@
+function weirdMultiply(sentence) {
+var stringNumber = sentence.toString();
+
+if (stringNumber.length == 1)
+return Number(stringNumber);
+var hasil = 1;
+
+for (let i =0;i < stringNumber.length;i++)
+hasil = hasil * Number (stringNumber[i])
+return weirdMultiply(hasil)
+}
+
+console.log(weirdMultiply(39));
+console.log(weirdMultiply(999));
+console.log(weirdMultiply(3));
