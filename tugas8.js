cc3932f1abfc78f9947d0a3e97489ab2d3292742
farraz13 <farraz.f13@gmail.com>
2022-09-13 20:00:35 +0700

    c

--- tugas8.js
+++ tugas8.js
@@ -1,23 +1,23 @@
 function pola(str) {
-var splitNumber = str.split(' ')
+    var splitNumber = str.split(' ')
 
-var getFirstNumber = splitNumber[0]
-var getSecNumber = splitNumber[2]
-var getThirdNumber = splitNumber[4]
+    var getFirstNumber = splitNumber[0]
+    var getSecNumber = splitNumber[2]
+    var getThirdNumber = splitNumber[4]
 
-var result = []
+    var result = []
 
-for (let i = 0; i <10;i++){
-    var changeIndex1 =getFirstNumber.replace('#',i);
-    for (let j =0; j<10;j++){
-        var changeIndex3 = getThirdNumber.replace('#',j)
-        if(Number (changeIndex1)* getSecNumber== Number (changeIndex3)){
-            result.push(i,j)
+    for (let i = 0; i < 10; i++) {
+        var changeIndex1 = getFirstNumber.replace('#', i);
+        for (let j = 0; j < 10; j++) {
+            var changeIndex3 = getThirdNumber.replace('#', j)
+            if (Number(changeIndex1) * getSecNumber == Number(changeIndex3)) {
+                result.push(i, j)
+            }
+
+
         }
-
-    
-    }
-}return result
+    } return result
 }
 console.log(pola('42#3 * 188 = 80#204'))
 console.log(pola('8#61 * 895 = 78410#5'))
