32e0a05ca6aa1907baf599e5506517b5a77999ff
farraz13 <farraz.f13@gmail.com>
2022-09-13 11:18:35 +0700

    c

--- tugas8.js
+++ tugas8.js
@@ -1,14 +1,14 @@
 function pola(str) {
 var splitNumber = str.split(' ')
 
-var getFirstIndex = splitNumber[0]
+var getFirstNumber = splitNumber[0]
 var getSecNumber = splitNumber[2]
 var getThirdNumber = splitNumber[4]
 
 var result = []
 
 for (let i = 0; i <10;i++){
-    var changeIndex1 =getFirstIndex.replace('#',i);
+    var changeIndex1 =getFirstNumber.replace('#',i);
     for (let j =0; j<10;j++){
         var changeIndex3 = getThirdNumber.replace('#',j)
         if(Number (changeIndex1)* getSecNumber== Number (changeIndex3)){
